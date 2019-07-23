7d7b81d4089aac06b3388ad2bab05440316b26a1
Akash Vishwakarma <14cse031giet@gmail.com>
2019-07-23 19:38:24 +0530

    Moving components to separate folder

--- Frontend/components/counter.jsx
+++ Frontend/components/counter.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import * as actions from './action/actions';
+import * as actions from '../action/actions';
 import { connect } from 'react-redux';
 
 // React component
--- Frontend/index.js
+++ Frontend/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import store from './store/store';
 import { Provider } from 'react-redux';
-import App from './counter.jsx';
+import App from './components/counter.jsx';
 
 ReactDOM.render(
   <Provider store={store}>
